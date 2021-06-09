5771a2f181ce208e3cb9bd5da595fd760cc5ae81
Morris Kyn <morris.kyn@gmail.com>
2021-06-09 10:57:51 -0400

    Bugs spawning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import beetle from './beetle.jpg';
 import './App.css';
+import React from 'react';
+
+function Bug(props){
+  return (
+    <span>
+    <img src={beetle} width="100" height="100" onClick={props.onClick}></img>
+    <div>{props.id}</div>
+    </span>
+  )
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bugs:[],
+      n:0,
+    }
+  }
+
+  componentWillMount(){
+    setInterval(this.newBug.bind(this),1000)
+  }
+
+  newBug() {
+    var newBugs=this.state.bugs;
+    var i = this.state.n;
+    newBugs.push(<Bug onClick={()=> this.handleClick(i)} id={i}></Bug>)
+    this.setState({
+      bugs:newBugs,
+      n:this.state.n+1
+    })
+  }
+
+  handleClick(n) {
+    var newBugs=this.state.bugs;
+    for (var i=0;i<this.state.bugs.length;i++) {
+      if (this.state.bugs[i].props.id == n) {
+        newBugs.splice(i,1);
+      }
+    }
+    this.setState({
+      bugs:newBugs,
+    })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.bugs}
+      </ul>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Game></Game>
   );
 }
 
